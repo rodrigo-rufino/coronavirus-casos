755ad4a3dbf7c17e962dbd0c1ec164101240a952
rodrigo-rufino <rodrigo.srs.br@gmail.com>
2020-05-06 20:59:30 -0300

    Revert "revert"
    This reverts commit 0eb0623783f476ba568671e514e474c260474774.

--- client/src/components/SimpleLineChart/index.js
+++ client/src/components/SimpleLineChart/index.js
@@ -1,52 +1,31 @@
 import React, { PureComponent } from 'react';
 import {
-  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer
 } from 'recharts';
 
-const data = [
-  {
-    name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
-  },
-  {
-    name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-  },
-  {
-    name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-  },
-  {
-    name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-  },
-  {
-    name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-  },
-  {
-    name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-  },
-  {
-    name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-  },
-];
-
 export default class Example extends PureComponent {
+  static jsfiddleUrl = 'https://jsfiddle.net/alidingling/xqjtetw0/';
 
   render() {
     return (
-      <LineChart
-        width={500}
-        height={300}
-        data={data}
-        margin={{
-          top: 5, right: 30, left: 20, bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-      </LineChart>
+      <ResponsiveContainer width="95%" height={400}>
+        <LineChart
+          data={this.props.data}
+          margin={{
+            top: 20, right: 20, left: 20, bottom: 20,
+          }}
+          style={{height: 1000}}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="data" />
+          <YAxis/>
+          <Tooltip />
+          <Legend />
+          <Line name="Total de Casos" type="monotone" dataKey="totalCasos" stroke="#8884d8" strokeWidth={2} activeDot={{ r: 5 }} dot={false}/>
+          <Line name="Total de Óbitos" type="monotone" dataKey="totalObitos" stroke="#82ca9d" strokeWidth={2} dot={false}/>
+          <Line name="Estimativa" type="monotone" dataKey="estimativa" stroke="#a8a5e6" strokeDasharray="5 5" strokeWidth={1} dot={false}/>
+        </LineChart>
+      </ResponsiveContainer>
     );
   }
 }
